0c612c74f3932e9b76c3546c12b86d07c643c6d5
OVO <65055474+OVOU4@users.noreply.github.com>
2022-09-09 00:42:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,8 +29,8 @@ sed -i 's/OpenWrt /OpenWrt @ 洋洋姐姐 /g' package/lean/default-settings/file
 #sed -i '/KERNEL_PATCHVER/cKERNEL_PATCHVER:=5.10' target/linux/ramips/Makefile
 
 
-#target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
-#for configFile in $(ls target/linux/$target/config*)
-#do
-#    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
-#done
+target=$(grep "^CONFIG_TARGET" .config --max-count=1 | awk -F "=" '{print $1}' | awk -F "_" '{print $3}')
+for configFile in $(ls target/linux/$target/config*)
+do
+    echo -e "\nCONFIG_NETFILTER_NETLINK_GLUE_CT=y" >> $configFile
+done
